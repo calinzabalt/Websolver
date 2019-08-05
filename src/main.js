cd0b4c24f4063f4d24e582ff93e61d76c2a8058f
calinzabalt <calinzabalt@gmail.com>
2019-08-05 16:03:27 +0300

    on

--- src/main.js
+++ src/main.js
@@ -13,19 +13,6 @@ Vue.use(VueAnalytics, {
 })
 Vue.use(Vuetify)
 
-const prod = process.env.NODE_ENV === 'production'
-const shouldSW = 'serviceWorker' in navigator && prod
-const shouldSWDev = 'serviceWorker' in navigator && !prod
-if (shouldSW) {
-  navigator.serviceWorker.register('/service-worker.js').then(() => {
-    console.log("Service Worker Registered!")
-  })
-}
-else if (shouldSWDev) {
-  navigator.serviceWorker.register('/service-workder-dev.js').then(() => {
-    console.log('Service Worker Registered!')
-  })
-}
 Vue.config.productionTip = false
 
 new Vue({
